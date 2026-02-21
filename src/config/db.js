e7cb91b38168a153a8f67fc1857f932df56fd6b4
Savinay76 <savinayreddysathi172@gmail.com>
2026-02-21 17:04:13 +0530

    feat: add Sequelize database configuration with environment variable support

---
+++ src/config/db.js
@@ -0,0 +1,9 @@
+const { Sequelize } = require('sequelize');
+require('dotenv').config();
+
+const sequelize = new Sequelize(process.env.DATABASE_URL, {
+  dialect: 'postgres',
+  logging: false, // Set to true if you want to see SQL queries in console
+});
+
+module.exports = sequelize;
